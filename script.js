4264b9ef4e45a8cfd53fb69e415303ec1a42b3db
gnutel3 <gogalol2017@yandex.ru>
2025-12-14 17:08:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,8 @@
 // Плавная прокрутка
-document.querySelectorAll('nav a[href^="#"]').forEach(link => {
-    link.addEventListener("click", function (e) {
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener("click", e => {
         e.preventDefault();
-        const id = this.getAttribute("href");
-        document.querySelector(id).scrollIntoView({
-            behavior: "smooth"
-        });
-    });
-});
-
-// Обработка формы
-document.getElementById("contactForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-
-    const email = document.getElementById("email").value;
-
-    if (!email.includes("@")) {
-        document.getElementById("formStatus").textContent = "Введите корректный email!";
-        return;
-    }
-
-    document.getElementById("formStatus").textContent = "Сообщение успешно отправлено!";
-    this.reset();
-});
-
-// Кнопка купить
-document.querySelectorAll(".buy-btn").forEach(button => {
-    button.addEventListener("click", () => {
-        alert("Спасибо за покупку! Наш менеджер скоро свяжется с вами.");
+        document.querySelector(link.getAttribute("href"))
+            .scrollIntoView({ behavior: "smooth" });
     });
 });
